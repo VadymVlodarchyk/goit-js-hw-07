c010e1438b034ca689513a7fae3cd25f16b571bd
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2024-01-25 19:52:39 +0000

    yuy

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const formElem = document.querySelector('.login-form');
+formElem.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const userEmail = formElem.elements.email.value.trim();
+    const userPassword = formElem.elements.password.value.trim();
+    const obj = {
+        email: userEmail,
+        password: userPassword,
+    };
+    console.log(obj);
+    if (userEmail === '' || userPassword === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+    formElem.reset();
+};
+
+
+
 
