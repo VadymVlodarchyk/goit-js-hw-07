fc80741f8e7fb0b4cb9458b478d4d31311a47290
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2024-01-26 18:27:08 +0000

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,36 @@
+const backgroundColor = document.querySelector('.color');
+const batton = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+batton.addEventListener('click', changeColor);
+function changeColor() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  backgroundColor.textContent = newColor;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
